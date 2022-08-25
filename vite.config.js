132b6895d6225e0aeb0e626bcf07351f90b1fe80
Jerred Wernke <jerredwernke@Jerreds-MBP.lan1>
2022-08-25 12:54:55 -0400

    feat: fixing vite to work with my docker setup

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+        host: '0.0.0.0',
+        port: 5173,
+    },
     plugins: [
         laravel({
             input: [
